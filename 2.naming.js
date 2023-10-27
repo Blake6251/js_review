d05dffde5dc0d79092b73ab1c33b1eb7b1b55fa8
yhh <yhh6251@gmail.com>
2023-10-27 10:24:01 +0900

    10.27

---
+++ 2.naming.js
@@ -0,0 +1,33 @@
+// 주석을 달면 됨
+/**
+ * 연속 주석
+ */
+/** 변수 규칙!
+ * 라틴문자(0-9, a-z, A-Z,), _
+ * 대소문자를 구별함
+ * 추천 : camelCase 
+ * 한국어 절대 안됨
+ * 예약어 X let if;
+ * 숫자로 시작 X
+ * 특수문자 _, $ 예외
+ * 이모지 X
+ */
+
+let apple;
+let redApple;
+
+// 나쁜 예제
+let number = 20;
+let audio1;
+let audio2;
+
+
+// 좋은 예제
+let myAge =20;
+let backgroundAudio;
+let windAudio;
+
+// 팁
+let audioBackground;
+let audioWind;
+

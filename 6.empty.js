417f4173ce04bdeae3869a616805bd10e4918948
yhh <yhh6251@gmail.com>
2023-10-30 13:51:14 +0900

    1030

--- 6.empty.js
+++ 6.empty.js
@@ -4,3 +4,11 @@ console.log(variable);
 
 variable = null;
 console.log(variable);
+
+let activeItem; // 아직 활성화된 아이템이 있는지 없는지 모르는 상태!
+activeItem = null; // 활성화된 아이템이 없는 상태!
+
+console.log(typeof 123);
+console.log(typeof '123');
+console.log(typeof null);   // object
+console.log(typeof undefined); // undefined
